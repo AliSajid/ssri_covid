8c9b9c49a606b35fc8fda98b14700ccdeaf0a9a2
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-03-29 21:05:46 -0400

    Reworked to include newer drugs and remove unusable ones. Also removed filters.

--- curate_signatures.R
+++ curate_signatures.R
@@ -1,24 +1,22 @@
 library(tidyverse)
 
-common_cell_lines <- c("A375", "MCF7")
+fluoxetine <- read_tsv("signature_data/Fluoxetine-Signatures.tsv")
 
-fluoxetine <- read_tsv("signature_data/Fluoxetine-Signatures.tsv") %>% 
-  filter(is_exemplar == 1, CellLine %in% common_cell_lines)
-escitalopram <- read_tsv("signature_data/Escitalopram-Signatures.tsv") %>% 
-  filter(Perturbagen == "Escitalopram", is_exemplar == 1, CellLine %in% common_cell_lines)
-sertraline <- read_tsv("signature_data/Sertraline-Signatures.tsv") %>% 
-  filter(is_exemplar == 1, Perturbagen == "Sertraline", CellLine %in% common_cell_lines)
+bupropion <- read_tsv("signature_data/Bupropion-Signatures.tsv")
+
+paroxetine <- read_tsv("signature_data/Paroxetine-Signatures.tsv")
+
 
 IL6 <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6", is_exemplar == 1, CellLine %in% common_cell_lines)
+  filter(TargetGene == "IL6")
 
 IL6R <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6R", is_exemplar == 1, CellLine %in% common_cell_lines)
+  filter(TargetGene == "IL6R")
 
 IL6ST <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6ST", is_exemplar == 1, CellLine %in% common_cell_lines)
+  filter(TargetGene == "IL6ST")
 
-drugs <- bind_rows(fluoxetine, escitalopram, sertraline)
+drugs <- bind_rows(fluoxetine, bupropion, paroxetine)
 write_file(paste(drugs$SignatureId, collapse = "\n"), "drugs_signature_ids")
 targets <- bind_rows(IL6, IL6R, IL6ST)
 write_file(paste(targets$SignatureId, collapse = "\n"), "targets_signature_ids")
