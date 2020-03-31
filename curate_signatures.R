b2f420db4d68dbb9f99aa49b221f5d990b0d6696
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-03-31 09:59:23 -0400

    Made the signature selection better.

--- curate_signatures.R
+++ curate_signatures.R
@@ -1,21 +1,27 @@
 library(tidyverse)
 
-fluoxetine <- read_tsv("signature_data/Fluoxetine-Signatures.tsv")
+cell_lines <- c("A375", "A549", "HA1E", "HCC515", "HEPG2", "HT29", "MCF7", "PC3")
 
-bupropion <- read_tsv("signature_data/Bupropion-Signatures.tsv")
+fluoxetine <- read_tsv("signature_data/Fluoxetine-Signatures.tsv") %>% 
+  filter(CellLine %in% cell_lines)
 
-paroxetine <- read_tsv("signature_data/Paroxetine-Signatures.tsv")
+bupropion <- read_tsv("signature_data/Bupropion-Signatures.tsv") %>% 
+  filter(CellLine %in% cell_lines)
 
-dexa <- read_tsv("signature_data/Dexamethasone-Signatures.tsv")
+paroxetine <- read_tsv("signature_data/Paroxetine-Signatures.tsv") %>% 
+  filter(CellLine %in% cell_lines)
+
+dexa <- read_tsv("signature_data/Dexamethasone-Signatures.tsv") %>% 
+  filter(CellLine %in% cell_lines)
 
 IL6 <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6")
+  filter(TargetGene == "IL6", CellLine %in% cell_lines)
 
 IL6R <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6R")
+  filter(TargetGene == "IL6R", CellLine %in% cell_lines)
 
 IL6ST <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6ST")
+  filter(TargetGene == "IL6ST", CellLine %in% cell_lines)
 
 drugs <- bind_rows(fluoxetine, bupropion, paroxetine, dexa)
 write_file(paste(drugs$SignatureId, collapse = "\n"), "drugs_signature_ids")
