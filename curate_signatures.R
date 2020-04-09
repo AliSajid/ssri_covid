8d8af2966e94630c66456637607e1511c152181c
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-04-09 18:46:42 -0400

    Added newer drugs to curate signatures. Also automated creating the creation of id-name-cell-mapping

--- curate_signatures.R
+++ curate_signatures.R
@@ -14,6 +14,12 @@ paroxetine <- read_tsv("signature_data/Paroxetine-Signatures.tsv") %>%
 dexa <- read_tsv("signature_data/Dexamethasone-Signatures.tsv") %>% 
   filter(CellLine %in% cell_lines)
 
+hydroxychloroquine <- read_tsv("signature_data/Hydroxychloroquine-Signatures.tsv") %>% 
+  filter(CellLine %in% cell_lines)
+
+chloroquine <- read_tsv("signature_data/Chloroquine-Signatures.tsv") %>% 
+  filter(CellLine %in% cell_lines)
+
 IL6 <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
   filter(TargetGene == "IL6", CellLine %in% cell_lines)
 
@@ -23,7 +29,9 @@ IL6R <- read_tsv("signature_data/IL6-Signatures.tsv") %>%
 IL6ST <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
   filter(TargetGene == "IL6ST", CellLine %in% cell_lines)
 
-drugs <- bind_rows(fluoxetine, bupropion, paroxetine, dexa)
+drugs <- bind_rows(fluoxetine, bupropion, paroxetine, dexa, hydroxychloroquine, chloroquine)
 write_file(paste(drugs$SignatureId, collapse = "\n"), "drugs_signature_ids")
 targets <- bind_rows(IL6, IL6R, IL6ST)
 write_file(paste(targets$SignatureId, collapse = "\n"), "targets_signature_ids")
+drugs %>% select(SignatureId, Perturbagen, CellLine) %>% 
+  write_csv("signature_data/id-name-cellline_mapping.csv")
