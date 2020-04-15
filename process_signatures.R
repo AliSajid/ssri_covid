9fdf9b7f2c3e048fb2daf553165fc5ef802732c5
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-04-15 14:34:00 -0400

    Cleanedd up process_signatures

--- process_signatures.R
+++ process_signatures.R
@@ -1,29 +1,20 @@
 library(tidyverse)
-library(httr)
-library(jsonlite)
 source("pipeline.R")
 
 drugs_signatures <- unlist(strsplit(read_file("drugs_signature_ids"), split = "\n"))
-targets_signatures <- unlist(strsplit(read_file("targets_signature_ids"), split = "\n"))
 
 for (drug in drugs_signatures) {
   prefix <- paste("data", "signatures", sep = "/")
   filename <- paste(paste(drug, "Signature", sep = "-"), "tsv", sep = ".")
   fullname <- paste(prefix, filename, sep = "/")
   if (!file.exists(fullname)) {
+    print(paste("Now Processing Signature:", drug))
     sig <- get_l1000_signature(drug)
     write_tsv(sig, path = fullname)
+  } else {
+    print(paste(drug, "Signature already exists."))
   }
 }
-
-for (target in targets_signatures) {
-  prefix <- paste("data", "signatures", sep = "/")
-  filename <- paste(paste(target, "Signature", sep = "-"), "tsv", sep = ".")
-  fullname <- paste(prefix, filename, sep = "/")
-  if (!file.exists(fullname)) {
-    sig <- get_l1000_signature(target)
-    write_tsv(sig, path = fullname)
-  }}
 
 for (drug in drugs_signatures) {
   prefix <- paste("data", "signatures", sep = "/")
@@ -41,46 +32,24 @@ for (drug in drugs_signatures) {
   
 }
 
-# for (target in targets_signatures) {
-#   prefix <- paste("data", "signatures", sep = "/")
-#   filename <- paste(paste(target, "Signature", sep = "-"), "tsv", sep = ".")
-#   fullname <- paste(prefix, filename, sep = "/")
-#   up <- generate_filtered_signature(signature_file = fullname, direction = "up")
-#   down <- generate_filtered_signature(signature_file = fullname, direction = "down")
-#   consensus <- bind_rows(up, down)
-#   
-#   consensus_prefix <- paste("data", "filtered", sep = "/")
-#   consensus_filename <- paste(paste(target, "Consensus", "Signature", sep = "-"), "tsv", sep = ".")
-#   consensus_fullname <- paste(consensus_prefix, consensus_filename, sep = "/")
-#   
-#   write_tsv(consensus, consensus_fullname)
-# }
-
 drug_results <- list.files("data/filtered/", pattern = "LINCSCP", full.names = TRUE)
-#target_results <- list.files("data/filtered/", pattern = "LINCSKD", full.names = TRUE)
 
-genes <- c("IL6", "IL6R", "IL6ST")
+genes <- c("IL6", "IL6R", "IL6ST", "NFKB1", "NFKB2")
 
 for (drug in drug_results) {
   name <- str_split(drug, "/")[[1]][4]
   name <- str_split(name, "-")[[1]][1]
-  concordance <- get_concordant_signatures(drug, library = "LIB_6") %>% 
-  filter(treatment %in% genes) %>% 
-  mutate(Source_Signature = name)
   prefix <- paste("results", "drugs", sep = "/")
   filename <- paste(paste(name, "Concordant", sep = "-"), "tsv", sep = ".")
   fullname <- paste(prefix, filename, sep = "/")
-  write_tsv(concordance, fullname)
+  if (!file.exists(fullname)) {
+    print(paste("Processing concordance for signature:", name))
+    concordance <- get_concordant_signatures(drug, library = "LIB_6") %>% 
+      filter(treatment %in% genes) %>% 
+      mutate(Source_Signature = name)
+    write_tsv(concordance, fullname)
+  } else {
+    print(paste(name, "concordant signature already exists. Skipping."))
+  }
+  
 }
-
-# drugs <- c("Fluoxetine", "Paroxetine", "Bupropion")
-# 
-# for (target in target_results) {
-#   name <- str_split(target, "/")[[1]][4]
-#   concordance <- get_concordant_signatures(target) %>% 
-#     filter(compound %in% drugs)
-#   prefix <- paste("results", "targets", sep = "/")
-#   filename <- paste(paste(name, "Concordant", sep = "-"), "tsv", sep = ".")
-#   fullname <- paste(prefix, filename, sep = "/")
-#   write_tsv(concordance, fullname)
-# }
