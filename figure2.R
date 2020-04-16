e9fb6b7aef413e2a218f329fb7022de11b3e9db8
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-04-15 22:38:11 -0400

    Better Heatmaps

--- figure2.R
+++ figure2.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(gplots)
 
 genes <- c("IL6", "IL6R", "IL6ST", "NFKB1", "NFKB2", "TNF", "RELA", "RELB", "REL", "CREL")
 
@@ -23,9 +24,17 @@ for (g in genes) {
   full_name <- paste("figures", file_name, sep = "/")
   if (g %in% names(data)) {
     d <- data[[g]]
-    heatmap(d, Colv = NA, Rowv = NA, scale = "column", col = colors, main = g)
+    heatmap.2(d, dendrogram = "none", col = colors, colsep = 1:(dim(d)[2] - 1), rowsep = 1:(dim(d)[1] - 1),
+              trace = "none", cexRow = 3, cexCol = 3, Rowv = FALSE, Colv = FALSE,
+              density.info = "none", keysize = 1, margins = c(15, 25), notecex=2.0,
+              main = paste("Concordance across cell lines for", g),
+              key.title = NA, key.xlab = NA, key.par = list(cex = 1.3), cellnote = d, notecol = "white")
     png(filename = full_name, width = 1920, height = 1384)
-    heatmap(d, Colv = NA, Rowv = NA, scale = "column", col = colors, main = g)
+    heatmap.2(d, dendrogram = "none", col = colors, colsep = 1:(dim(d)[2] - 1), rowsep = 1:(dim(d)[1] - 1),
+              trace = "none", cexRow = 3, cexCol = 3, Rowv = FALSE, Colv = FALSE,
+              density.info = "none", keysize = 1, margins = c(15, 25), notecex=2.0,
+              main = paste("Concordance across cell lines for", g),
+              key.title = NA, key.xlab = NA, key.par = list(cex = 1.3), cellnote = d, notecol = "white")
     dev.off()
   }
 }
